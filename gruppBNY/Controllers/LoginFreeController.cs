4ea35130492bfd2c4856fb9c056270a1b5814168
Adam Enmark <aden1800@student.miun.se>
2019-11-01 11:02:31 +0100

    Login sidor fixade.

---
+++ gruppBNY/Controllers/LoginFreeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gruppBNY.Controllers
+{
+    public class LoginFreeController : Controller
+    {
+        // GET: LoginFree
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
