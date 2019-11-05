8476b4b2822600d195ad087233595fd64e60ec67
Jimmy Floresten <JimmyFloresten@hotmail.se>
2019-11-05 13:58:24 +0100

    Gjort lite

--- gruppBNY/Controllers/competencesController.cs
+++ gruppBNY/Controllers/competencesController.cs
@@ -61,33 +61,48 @@ namespace gruppBNY.Controllers
         }
 
         // GET: competences/Edit/5
-        public ActionResult Edit(int? id)
+        public ActionResult Edit(int? id, int? id2)
         {
+            //    if (id == null)
+            //    {
+            //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //    }
+            //    var competenceVM = new competenceVM
+            //    {
+            //        competence = db.competence.Include(i => i.category).First(i => i.competence_Id == id)
+            //    };
+
+
+
+            //    if (competenceVM.competence == null)
+            //    {
+            //        return HttpNotFound();
+            //    }
+            //    var catList = db.category.ToList();
+            //    competenceVM.Allcategories = catList.Select(o => new SelectListItem
+            //    {
+            //        Text = o.type_of_competence,
+            //        Value = o.category_Id.ToString()
+            //    });
+            //    ViewBag.freelancer_Id = new SelectList(db.Freelancer, "freelancer_Id", "Firstname", competenceVM.competence.freelancer_id);
+
+
+            //    return View(competenceVM);
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            var competenceVM = new competenceVM
-            {
-                competence = db.competence.Include(i => i.category).First(i => i.competence_Id == id)
-            };
+       
 
-
-
-            if (competenceVM.competence == null)
+            competence competence = db.competence.Find(id);
+            
+            if (competence == null)
             {
                 return HttpNotFound();
             }
-            var catList = db.category.ToList();
-            competenceVM.Allcategories = catList.Select(o => new SelectListItem
-            {
-                Text = o.type_of_competence,
-                Value = o.category_Id.ToString()
-            });
-            ViewBag.freelancer_Id = new SelectList(db.Freelancer, "freelancer_Id", "Firstname", competenceVM.competence.freelancer_id);
+        
             
-         
-            return View(competenceVM);
+            return View(competence);
         }
 
         // POST: competences/Edit/5
@@ -95,7 +110,7 @@ namespace gruppBNY.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "competence_Id,competences,scale")] competence competence)
+        public ActionResult Edit([Bind(Include = "competence_Id,competences,scale, freelancer_Id")] competence competence)
         {
             if (ModelState.IsValid)
             {
